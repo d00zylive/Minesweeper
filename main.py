da7bae8b4e3dc7c2ad2e209c1b6126a3484d1bfa
Doozy <d00zyl1v3@gmail.com>
2026-05-27 22:30:08 +0200

    Added mine method Also changed value to count and created method getAdjacentCells

--- main.py
+++ main.py
@@ -7,31 +7,47 @@ class Cell():
     x: int
     y: int
     grid: list[list[Self]]
-    value: int|None #0-8 or -1 for mine
+    count: int|None #0-8 or -1 for mine
     flagged: bool
     mined: bool
 
-    def __init__(self, x: int, y: int, grid: list[list[Self]], flagged:bool = False, mined:bool = False, value:int|None = None):
+    def __init__(self, x: int, y: int, grid: list[list[Self]], flagged:bool = False, mined:bool = False, count:int|None = None):
         self.x = x
         self.y = y
         self.grid = grid
-        self.value = value
+        self.count = count
         self.flagged = flagged
         self.mined = mined
 
     def __repr__(self):
-        return f"Cell(x={self.x},y={self.y},value={self.value},flagged={self.flagged}, mined={self.mined})"
-        
-    def intialiseValue(self) -> None:
-        if self.value != -1:
-            self.value = 0
-            for vector in ADJACENCYVECTORS:
-                x = self.x+vector[0]
-                y = self.y+vector[1]
-                if x < 0 or y < 0 or x >= len(self.grid) or y >= len(self.grid[0]):
-                    continue
-                if self.grid[x][y].value == -1:
-                    self.value += 1
+        return f"Cell(x={self.x},y={self.y},count={self.count},flagged={self.flagged}, mined={self.mined})"
+
+    def getAdjacentCells(self) -> list[Self]:
+        adjacentCells = []
+        for vector in ADJACENCYVECTORS:
+            x = self.x+vector[0]
+            y = self.y+vector[1]
+            if x < 0 or y < 0 or x >= len(self.grid) or y >= len(self.grid[0]):
+                continue
+            adjacentCells.append(self.grid[x][y])
+        return adjacentCells
+            
+
+    def intialiseCount(self) -> None:
+        if self.count != -1:
+            self.count = 0
+            for cell in self.getAdjacentCells():
+                if cell.count == -1:
+                    self.count += 1
+    
+    def mine(self) -> bool:
+        if not self.mined:
+            self.mined = True
+            if self.count == 0:
+                for cell in self.getAdjacentCells():
+                    cell.mine()
+            return self.count == -1
+        return False
             
 
 def initialiseGrid(width:int, height:int, mines:int) -> list[list[Cell]]:
@@ -44,12 +60,12 @@ def initialiseGrid(width:int, height:int, mines:int) -> list[list[Cell]]:
 
     for _ in range(mines):
             cell = grid[random.randint(0,width-1)][random.randint(0,height-1)]
-            if cell.value == None:
-                cell.value = -1
+            if cell.count == None:
+                cell.count = -1
 
     for column in grid:
         for cell in column:
-            cell.intialiseValue()
+            cell.intialiseCount()
 
     return grid
 
@@ -57,10 +73,10 @@ def printGrid(grid:list[list[Cell]], ignoreMined: bool = False) -> None:
     for column in grid:
         for cell in column:
             if cell.mined or ignoreMined:
-                if cell.value == -1:
+                if cell.count == -1:
                     print("@",end="")
                 else:
-                    print(cell.value,end="")
+                    print(cell.count,end="")
             elif cell.flagged:
                 print("F",end="")
             else:
@@ -68,3 +84,10 @@ def printGrid(grid:list[list[Cell]], ignoreMined: bool = False) -> None:
         print()
 
 grid = initialiseGrid(width=10, height=10, mines=10)
+
+printGrid(grid, True)
+printGrid(grid)
+while True:
+    random.choice(random.choice(grid)).mine()
+    printGrid(grid)
+    input()
